619c5aed106619df50f52243626f8b8c1779a4a2
davideboun21 <davideboun@gmail.com>
2022-09-08 18:09:24 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
  * main - Printing
  *
  * Return: Always 0 (Successful)
- * */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual 
-puzzle");
-	return (0);
-	
-}	
+ */
+ int main(void)
+ {
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return(0);
+ }	 
+		
